95bc2a4e1d13f47c829285f9bcb4ca4a7e6a1e2a
Muhamad Airi <minoramu@gmail.com>
2020-08-18 07:01:56 +0700

    Adding overlay

--- src/containers/Demo/Demo.js
+++ src/containers/Demo/Demo.js
@@ -20,8 +20,18 @@ class Demo extends Component{
               </div>
           </div>
           </div>
-          <div className={classes.firstImageContainer}> <p>Keluarkan Raga Dewa. Dengan jurus anti maut. Membuat anda menjadi perkasa, tanpa obat. Majur, membuat anda jago main badminton. Bisa dipakai untuk berkendara. Hati - Hati jauhkan dari anak anak, Bahaya jika sampai tertelan. </p> </div>
+          <div className={classes.firstImageContainer}> <p>Keluarkan Raga Dewa. Dengan jurus anti maut. Membuat anda menjadi perkasa, tanpa obat. Majur, membuat anda jago main badminton. Bisa dipakai untuk berkendara. Hati - Hati jauhkan dari anak anak, Bahaya jika sampai tertelan. Keluarkan Raga Dewa. Dengan jurus anti maut. Membuat anda menjadi perkasa, tanpa obat. Majur, membuat anda jago main badminton. Bisa dipakai untuk berkendara. Hati - Hati jauhkan dari anak anak, Bahaya jika sampai tertelan. Keluarkan Raga Dewa. Dengan jurus anti maut. Membuat anda menjadi perkasa, tanpa obat. Majur, membuat anda jago main badminton. Bisa dipakai untuk berkendara. Hati - Hati jauhkan dari anak anak, Bahaya jika sampai tertelan. Keluarkan Raga Dewa. Dengan jurus anti maut. Membuat anda menjadi perkasa, tanpa obat. Majur, membuat anda jago main badminton. Bisa dipakai untuk berkendara. Hati - Hati jauhkan dari anak anak, Bahaya jika sampai tertelan. </p> </div>
           
+        </div>
+        <div className={classes.secondContent}>
+          
+          <div className={classes.secondImageContainer}> <p>AAAAKeluarkan Raga Dewa. Dengan jurus anti maut. Membuat anda menjadi perkasa, tanpa obat. Majur, membuat anda jago main badminton. Bisa dipakai untuk berkendara. Hati - Hati jauhkan dari anak anak, Bahaya jika sampai tertelan. Keluarkan Raga Dewa. Dengan jurus anti maut. Membuat anda menjadi perkasa, tanpa obat. Majur, membuat anda jago main badminton. Bisa dipakai untuk berkendara. Hati - Hati jauhkan dari anak anak, Bahaya jika sampai tertelan. Keluarkan Raga Dewa. Dengan jurus anti maut. Membuat anda menjadi perkasa, tanpa obat. Majur, membuat anda jago main badminton. Bisa dipakai untuk berkendara. Hati - Hati jauhkan dari anak anak, Bahaya jika sampai tertelan. Keluarkan Raga Dewa. Dengan jurus anti maut. Membuat anda menjadi perkasa, tanpa obat. Majur, membuat anda jago main badminton. Bisa dipakai untuk berkendara. Hati - Hati jauhkan dari anak anak, Bahaya jika sampai tertelan. </p> </div>
+          <div className={classes.secondImage}>
+            <img src={firstImage} alt="secondImage"/>
+            <div className={classes.overlay2}>
+              <div className={classes.overlayText2}> &lt; Go Nyebur </div>
+            </div>
+          </div>
         </div>
       </section>
       </div>
